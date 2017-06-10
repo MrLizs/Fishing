38bcdb3881c0035b5e81d2869db891ef82719446
MrLizs <ls_710@qq.com>
2017-06-10 17:27:30 +0800

    Ver 0.5.6

--- assets/Script/BarbManager.js
+++ assets/Script/BarbManager.js
@@ -27,21 +27,24 @@ cc.Class({
         for (var i = 0; i < theFishes.length; i++) {
             if(theFishes[i].node === other.node)
             {
-                if(theFishes[i].Floor === 1)
-                {
-                    this.score.getComponent(cc.Label).string = parseInt(this.score.getComponent(cc.Label).string) + 10;
-                    cc.log('第1层,类型'+theFishes[i].nodeClass);
-                }
-                else if(theFishes[i].Floor === 2)
-                {
-                    this.score.getComponent(cc.Label).string = parseInt(this.score.getComponent(cc.Label).string) + 30;
-                    cc.log('第2层,类型'+theFishes[i].nodeClass);
-                }
-                else if(theFishes[i].Floor === 3)
-                {
-                    this.score.getComponent(cc.Label).string = parseInt(this.score.getComponent(cc.Label).string) + 50;
-                    cc.log('第3层,类型'+theFishes[i].nodeClass);
-                }
+                var MyScore = theFishes[i].Floor * (theFishes[i].nodeClass * 10)
+                cc.log('增加积分' + MyScore);
+                this.score.getComponent(cc.Label).string = parseInt(this.score.getComponent(cc.Label).string) + MyScore;
+                // if(theFishes[i].Floor === 1)
+                // {
+                //     this.score.getComponent(cc.Label).string = parseInt(this.score.getComponent(cc.Label).string) + theFishes[i].nodeClass*10;
+                //     cc.log('第1层,类型'+theFishes[i].nodeClass);
+                // }
+                // else if(theFishes[i].Floor === 2)
+                // {
+                //     this.score.getComponent(cc.Label).string = parseInt(this.score.getComponent(cc.Label).string) + theFishes[i].nodeClass*10;
+                //     cc.log('第2层,类型'+theFishes[i].nodeClass);
+                // }
+                // else if(theFishes[i].Floor === 3)
+                // {
+                //     this.score.getComponent(cc.Label).string = parseInt(this.score.getComponent(cc.Label).string) + theFishes[i].nodeClass*10;
+                //     cc.log('第3层,类型'+theFishes[i].nodeClass);
+                // }
             }
         }
     },
